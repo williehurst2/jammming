5a1a8a235440620cc71089144918c391e1e3c412
williehurst2 <williehurst21@gmail.com>
2023-05-15 22:11:37 -0400

    filled out SearchBar

---
+++ src/App/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import SearchBar from '../SearchBar/SearchBar';
+
+function App() {
+  return (
+    <SearchBar />
+
+  );
+}
+
+export default App;
---
+++ src/SearchBar/SearchBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function SearchBar() {
+    return (
+        <div>
+            <p>Yerr</p>
+        </div>
+    )
+}
+export default SearchBar;
